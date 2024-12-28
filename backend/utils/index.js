d53304bdcd04df50d4d6b4a5a3da7390c23ba0d9
Gagan Bansal <bansalgagan2004@gmail.com>
2024-12-28 14:55:38 +0530

    "Backend Added"

---
+++ backend/utils/index.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
+
+const dbConnection = async() => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URL);
+        console.log("db connected");
+    } catch (error) {
+        console.log("Db connection failed : ", error);
+    }
+}
+
+export default dbConnection;
+
+export const createJWT = (res, userId) => {
+    const token = jwt.sign({userId}, process.env.JWT_SECRET, {expiresIn: "1D"});
+
+    res.cookie("token", token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV !== "development",
+        sameSite: "strict",
+        maxAge: 1 * 24 * 60 * 60 * 1000,
+    });
+}
